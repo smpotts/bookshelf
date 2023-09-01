65dbfbc5dcd545c926d822a3be712aea98e522b8
spotts685 <spotts685@gmail.com>
2023-09-01 07:47:56 -0600

    bookshelf project

---
+++ app/graphql/types/author_type.rb
@@ -0,0 +1,11 @@
+class Types::AuthorType < Types::BaseObject
+  description "One author"
+
+  field :id, ID, null: false
+  field :first_name, String, null: true
+  field :last_name, String, null: true
+  field :yob, Int, null: false
+  field :is_alive, Boolean, null: true
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+
+end
---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,28 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator" do
+      argument :name, String, required: true
+    end
+    def test_field(name:)
+      Rails.logger.info context[:time]
+      "Hello #{name}!"
+    end
+
+    field :author, Types::AuthorType, null: true, description: "Returns one author instance" do
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.where(id: id).first
+    end
+  end
+end
