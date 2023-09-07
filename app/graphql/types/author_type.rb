b462c5c839cef8ea2da53ffbdc9941b4eb183152
spotts685 <spotts685@gmail.com>
2023-09-07 10:18:12 -0600

    added publications and coords

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -8,4 +8,12 @@ class Types::AuthorType < Types::BaseObject
   field :is_alive, Boolean, null: true
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 
+  field :full_name, String, null: true
+  field :coordinates, Types::CoordinateType, null: true
+
+  field :publication_years, [Int], null: false
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
